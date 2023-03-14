4bca7c5545f40e93912f60820f1989667a232911
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-03-14 12:26:28 +0400

    #3 : create new method for show all countries in the API

--- src/src/APIConsumer.java
+++ src/src/APIConsumer.java
@@ -4,13 +4,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.google.gson.Gson;
 
-public class APIConsumer {
+public class APIConsumer<E> {
+	static ArrayList<String> countryList = new ArrayList<>();
 
-	public static void main(String[] args) {
-		String apiUrl = "http://universities.hipolabs.com/search?country=Oman";// United%20statesz
+	public static void ShowUniversitiesInCountryEntered() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\t\t\t\tEnter Country to pick all the Universities Details \n\t\t\t\tFor Ex.( United%20states OR Oman ):::");
+		String countryInput = sc.next();
+
+		sc.close();
+		String apiUrl = "http://universities.hipolabs.com/search?country=" + countryInput  ;// United%20states
 		try {
 			URL url = new URL(apiUrl);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -22,8 +31,8 @@ public class APIConsumer {
 			}
 
 			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-			String output;
-			StringBuilder json = new StringBuilder();
+			String output; // uncahngable -unmutable?
+			StringBuilder json = new StringBuilder(); // changable - mutable
 
 			while ((output = br.readLine()) != null) {
 				json.append(output);
@@ -37,7 +46,7 @@ public class APIConsumer {
 			// Use universities array for further processing
 			for (int i = 0; i < universities.length; i++) {
 				University university = universities[i];
-				System.out.println( (i+1) + ":" + university.state_province + " - " + university.country + " - "
+				System.out.println((i + 1) + ":" + university.state_province + " - " + university.country + " - "
 						+ university.name + " - " + university.alpha_two_code);
 
 				// iterate over domains
@@ -55,4 +64,50 @@ public class APIConsumer {
 			e.printStackTrace();
 		}
 	}
+	//--------------------------------------------------------------------------------------------------------------------------
+	public static void ShowAllCountryInAPI() {
+
+		String apiUrl = "http://universities.hipolabs.com/search?country="    ;
+		try {
+			URL url = new URL(apiUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			String output; // uncahngable -unmutable?
+			StringBuilder json = new StringBuilder(); // changable - mutable
+
+			while ((output = br.readLine()) != null) {
+				json.append(output);
+			}
+
+			conn.disconnect();
+
+			Gson gson = new Gson();
+			University[] universities = gson.fromJson(json.toString(), University[].class);
+	
+			
+			// Use universities array for further processing
+			int j = 0;
+			for (int i = 0; i < universities.length; i++) {
+				University university = universities[i];
+				if(!countryList.contains(university.country)) {
+					j++;
+					System.out.println(j + " - "+ university.country);
+					countryList.add(university.country);
+				}
+			};
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//--------------------------------------------------------------------------------------------------------------------------
+
+	
 }
